86ac75a8a9ded4d4efe1b009d4830da71453b3f6
Fabian Carlos <fabian.pow@gmail.com>
2013-04-05 05:26:45 -0400

    create new pessoa juridica and the products of then

--- Principal.php
+++ Principal.php
@@ -16,16 +16,35 @@ require_once 'Endereco.php';
 require_once 'exception/PessoaExcepiton.php'; 
 require_once 'Pessoa.php'; 
 require_once 'PessoaFisica.php';
-require_once 'Cliente.php'; 
+require_once 'PessoaJuridica.php';
+require_once 'Produto.php';
+require_once 'TipoProduto.php';
+require_once 'UnidadeMedida.php';
+require_once 'PessoaJuridicaProduto.php';
+require_once 'Cliente.php';
+
+$time = new DateTime();
 
 $estado = new Estado('Mato Grosso', 'MT', 'Centro-oeste');
 $cidade = new Cidade('Cuiaba', '65', $estado);
 $endereco = new Endereco('rua 2', 'centro', '78000-65', 'casa', '55', $cidade);
 
-$time = new DateTime();
+echo "PESSOA FISICA\n\n";
 
 $cliente = new Cliente('Fabian Carlos', $time, $endereco, '283.323.33-22', '323.546.782-32', 4336, 76);
 
-echo $cliente;
+echo $cliente . "\n";
 
+echo "PESSOA JURIDICA\n\n";
+
+$pessoa_juridiaca = new PessoaJuridica('Van Damme', $time, '12.123.123/0000-12', '12345', 'Empresa WEB BOM PINGA');
+
+$tipo_produto = new TipoProduto('PAPELARIA');
+$unidade_medida = new UnidadeMedida("Unidade","UN","");
+
+$produto = new Produto("Lapis", 130, 1.80, "", $unidade_medida, $tipo_produto);
+
+$pessoa_juridica_produto = new PessoaJuridicaProduto($pessoa_juridiaca, $produto);
+
+echo $pessoa_juridica_produto;
 
